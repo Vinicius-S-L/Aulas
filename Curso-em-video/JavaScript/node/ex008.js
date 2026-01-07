d3c6b288c6e91bf17ba1324396458a6f4a2ffd24
Vinicius-S-L <vinicius6060silva@gmail.com>
2026-01-07 15:37:27 -0300

    atividades

---
+++ Curso-em-video/JavaScript/node/ex008.js
@@ -0,0 +1,6 @@
+var vel = 60
+console.log(`A velocidade do carro foi ${vel} km/h`)
+if (vel > 60) {
+    console.log('Está acima da velocidade permitida, FOI MULTADO!!')
+}
+console.log('Use cinto de segurança')
